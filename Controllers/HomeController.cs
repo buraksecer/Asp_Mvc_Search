d2b74e0f906aedab5bf85a27fbac5f28ff48d697
burak seçer <buraksecer@users.noreply.github.com>
2018-03-12 01:58:02 +0300

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Search.Models;
+
+namespace Search.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        YemekSepeti yemek = new YemekSepeti();
+        databaseContext dbContext = new databaseContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult Index(object id)
+        {
+            if (id == null || id == "") id = 0;
+
+            int GelenId = Convert.ToInt32(id);
+
+            yemek = dbContext.ContactYemekSepeti.Where(b => b.Yemek_Id.Equals(GelenId)).FirstOrDefault();
+
+            if (yemek == null) yemek = new YemekSepeti();
+
+            return Json(yemek, JsonRequestBehavior.AllowGet);
+        }
+   
+    }
+}
---
+++ Models/YemekSepeti.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Search.Models
+{
+    public class YemekSepeti
+    {
+        [Key]
+        public int Yemek_Id { get; set; }
+
+        public String Yemek_Adi { get; set; }
+
+        public double Fiyat { get; set; }
+        public YemekSepeti()
+        {
+            Yemek_Id = 0;
+            Yemek_Adi = "Test";
+            Fiyat = 0;
+        }
+    }
+
+  
+
+    public class databaseContext:DbContext
+    {
+        public DbSet<YemekSepeti> ContactYemekSepeti { get; set; }
+    }
+
+  
+
+}    
